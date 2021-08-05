7db79182865cfd22d64b22346655a2d7869595f5
Ivan Zhukov <zhkvivan@gmail.com>
2021-08-05 15:29:21 +0100

    2 shopping list done

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,7 +1,14 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayOfGoods) {
+	let divWrap = document.querySelector('#content');
+	let ul = document.createElement('ul');
+	divWrap.appendChild(ul);
+	for (let i = 0; i < arrayOfGoods.length; i++) {
+		let li = document.createElement('li');
+		ul.appendChild(li);
+		li.textContent = arrayOfGoods[i];
+	}
 }
 
-let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
+let shopping = ['Milk', 'Bread', 'Eggs', 'A Dinosaur', 'Cake', 'Sugar', 'Tea'];
 
 shoppingList(shopping);
