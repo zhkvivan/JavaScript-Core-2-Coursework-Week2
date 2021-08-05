2d860af288089c82b7f59526caf71572e66fa191
Ivan Zhukov <zhkvivan@gmail.com>
2021-08-05 15:19:30 +0100

    exercise A done

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,11 +1,22 @@
 function listOfNames(arrayOfPeople) {
-  let content = document.querySelector("#content");
+	let content = document.querySelector('#content');
+	for (let i = 0; i < arrayOfPeople.length; i++) {
+		let name = document.createElement('h1');
+		let job = document.createElement('h2');
+
+		name.textContent = arrayOfPeople[i].name;
+		job.textContent = arrayOfPeople[i].job;
+
+		content.appendChild(name);
+		content.appendChild(job);
+
+	}
 }
 
 let people = [
-  { name: "Chris", job: "Teacher" },
-  { name: "Joanna", job: "Student" },
-  { name: "Boris", job: "Prime Minister" },
+	{ name: 'Chris', job: 'Teacher' },
+	{ name: 'Joanna', job: 'Student' },
+	{ name: 'Boris', job: 'Prime Minister' },
 ];
 
 listOfNames(people);
