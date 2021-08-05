e5e3d85cc48bfa44fa7315cb9a7486447842ffda
Ivan Zhukov <zhkvivan@gmail.com>
2021-08-05 17:32:22 +0100

    d todo list half done

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,32 @@
 function todoList(todos) {
-  // Write your code here...
+	let content = document.querySelector('#content');
+	let todoList = document.createElement('ul');
+	content.appendChild(todoList);
+	todos.forEach(object => {
+		let todoValue = document.createElement('li');
+		todoValue.className = 'todo-value';
+		todoList.appendChild(todoValue);
+		todoValue.textContent = object.todo;
+	});
+
+	let todoItems = document.querySelectorAll('.todo-value');
+	console.log(todoItems)
+	function lineThrough (li) {
+		if (!li.style.textDecoration) {
+			li.style.textDecoration = 'line-through';
+		};
+	};
+	todoItems.forEach(lineThrough)
+	//todoItems.forEach((todoItem) => {
+		//todoItem.addEventListener('click', lineThrough)
+	//})
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+	{ todo: 'wash the dishes' },
+	{ todo: 'walk the dog' },
+	{ todo: 'learn javascript' },
+	{ todo: 'go shopping' },
 ];
 
 todoList(todos);
