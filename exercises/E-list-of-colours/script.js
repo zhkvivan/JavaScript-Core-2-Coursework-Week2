7c502795e9e2a6353f78f28d93a8036d2a57a2ea
Ivan Zhukov <zhkvivan@gmail.com>
2021-08-06 08:50:42 +0100

    e list of colours done

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,7 +1,24 @@
 function listOfColours(colours) {
-  // Write your code here...
+	let content = document.querySelector('#content');
+
+	let dropdown = document.createElement('select');
+	content.appendChild(dropdown);
+
+	let p = document.createElement('p');
+	content.append(p);
+
+	colours.forEach((colour) => {
+		let option = document.createElement('option');
+		dropdown.appendChild(option);
+		option.textContent = colour;
+		option.value = colour;
+		dropdown.addEventListener('change', () => {
+			p.textContent = 'You have selected: ' + dropdown.value;
+			p.style.color = dropdown.value;
+		});
+	});
 }
 
-const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
+const colours = ['red', 'blue', 'green', 'yellow', 'pink', 'brown'];
 
 listOfColours(colours);
